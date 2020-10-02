094f049741f5070219b4907f63e79ef8404a2304
Tom Shea <thristhart@gmail.com>
2020-10-02 22:09:16 +0000

    basic infra

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "src"),
+
+    devtool: "source-map",
+
+    output: {
+        filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|mp3|wav)$/,
+                loader: "file-loader",
+            },
+        ],
+    },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+    },
+
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        // new CopyWebpackPlugin({
+        //     patterns: [{
+        //         from: "./assets/**/*"
+        //     }]
+        // }),
+    ],
+
+    node: {
+        fs: "empty",
+    },
+};
